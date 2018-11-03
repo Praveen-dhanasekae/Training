2be9a3ae56e8b12e4f35d7bfc378dd8f3e8d47cf
Praveen-dhanasekae <35263033+Praveen-dhanasekae@users.noreply.github.com>
2018-11-03 15:01:58 +0530

    Delete sql_pms.sql

--- Project -CTS/sql_pms.sql
+++
@@ -1,9 +0,0 @@
-create table policy_payment(
-
-bill_id varchar(20) primary key,
-user_id varchar(255) references mainuser(user_id),
-policy_id varchar references policy_table(policy_id),
-bill_date 
-)
-;
-
